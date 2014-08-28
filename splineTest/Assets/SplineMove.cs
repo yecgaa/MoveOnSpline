49549a569d750c9d29c80f628da951b29eb8b237
yecgaa <ripazha@ripazuha-no-MacBook-Pro.local>
2014-08-29 01:13:37 +0900

    レールチェンジできるよ。左から順に0,1,.....

--- splineTest/Assets/SplineMove.cs
+++ splineTest/Assets/SplineMove.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class SplineMove : MonoBehaviour{
 	public Spline spline=null;
 	public Spline[] splineList=null;
-	private int splineIndex=0 ;
-	private int rejectCount = 0;
+	private int splineIndex=0;
 	
 	public WrapMode wrapMode = WrapMode.Loop;
 	
@@ -17,32 +16,33 @@ public class SplineMove : MonoBehaviour{
 	public float DISTANCE_THRESHOLD=100f;
 	private float param=0f;
 
+	//numbering left to right 0-MAX
 	void Update(){
+		int tmpIndex = this.splineIndex;
 		if (Input.GetAxis ("Horizontal") > 0) {
 			Debug.Log ("Horizontal");
-			for(int i=0;i<this.splineList.Length;i++){
-				if(i==this.splineIndex){
-					Debug.Log ("continue");
-					continue;
-				}
-				Debug.Log(i);
-				Spline s=this.splineList[i];
-				float closeParam = s.GetClosestPointParam( this.transform.position, 5 );
-				float closeClampedParam = WrapValue (closeParam + offSet, 0f, 1f, wrapMode);
-				Vector3 closestPoint = s.GetPositionOnSpline( closeClampedParam );
-				float distance=(this.transform.position-closestPoint).magnitude;
-				//Debug.Log (distance);
-				if(distance<DISTANCE_THRESHOLD&&this.rejectCount<=0){
-					this.spline=s;
-					Debug.Log("spline change "+this.splineIndex+" to "+i);
-					this.splineIndex=i;
-					this.rejectCount=10;
-					this.transform.position=closestPoint;
-					param=closeParam;
-				}else{
-					Debug.Log (distance);
-					this.rejectCount--;
-				}
+			if (this.splineIndex < this.splineList.Length - 1) {
+					tmpIndex = this.splineIndex + 1;
+			}
+		} else if (Input.GetAxis ("Horizontal") < 0) {
+			Debug.Log ("Horizontal");
+			if (this.splineIndex > 0) {
+				tmpIndex = this.splineIndex - 1;
+			}
+		}
+		if (tmpIndex != this.splineIndex) {
+			Spline s = this.splineList [tmpIndex];
+			float closeParam = s.GetClosestPointParam (this.transform.position, 5);
+			float closeClampedParam = WrapValue (closeParam + offSet, 0f, 1f, wrapMode);
+			Vector3 closestPoint = s.GetPositionOnSpline (closeClampedParam);
+			float distance = (this.transform.position - closestPoint).magnitude;
+			//Debug.Log (distance);
+			if (distance < DISTANCE_THRESHOLD) {
+				this.spline = s;
+				Debug.Log ("spline change " + this.splineIndex + " to " + tmpIndex);
+				this.splineIndex=tmpIndex;
+				this.transform.position = closestPoint;
+				param = closeParam;
 			}
 		}
 		if (Input.GetAxis("Vertical")>0) {
